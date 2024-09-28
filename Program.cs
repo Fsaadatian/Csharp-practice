3e925821f4e6833a572cdd49ab3182f63aa3c24d
Fsaadatian <fa.saadatian@gmail.com>
2024-09-28 08:24:49 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Fsaadatian_Practice
+    // The aim of this program is to receive a user's profile, including first and last name, age and mobile number, and then print them.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, Enter your profile");
+            Console.WriteLine("First Name: ");
+            String? First_Name = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            String? Last_Name = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            String? Age = Console.ReadLine();
+            Console.WriteLine("Mobile Number: ");
+            String? Mobile_Number = Console.ReadLine();
+            if (String.IsNullOrEmpty(First_Name) || String.IsNullOrEmpty(Last_Name) || String.IsNullOrEmpty(Age) || String.IsNullOrEmpty(Mobile_Number))
+            {
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Console.WriteLine("Please, complete the deficiencies and then run the program again.");
+            }
+            else
+            {
+                Console.WriteLine("*******************************************************************************************************************************");
+                Console.WriteLine("Thank you, You are Mr./Mrs." + First_Name + " " + Last_Name + ", " + Age + " years old and your mobile number is " + Mobile_Number + ".");
+                Console.WriteLine("*******************************************************************************************************************************");
+            }
+
+        }
+    }
+}
